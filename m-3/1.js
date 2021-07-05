86e551e4c96c0bd729f84ab28608c6ac6828db5a
shixuewen <friendlysxw@163.com>
2021-07-05 16:18:35 +0800

    算法基础了解：end

---
+++ m-3/1.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-29 17:23:55
+ * @LastEditTime: 2021-06-29 17:29:10
+ * @LastEditors: Please set LastEditors
+ * @Description: 数组的查找
+ * @FilePath: \learn-my-first-algorithm-book\m-3\1.js
+ */
+
+/**
+ * 
+ * 1、线性查找
+ * 
+ * 按顺序查找,时间复杂度O(n)
+ * 
+ */
+
+/**
+ * 
+ * 2、二分查找
+ * 
+ * 只能查找已经排好序的数据
+ * 比较一次可以把查找范围缩小一半
+ * 
+ */
